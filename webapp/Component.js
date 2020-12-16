23fa469c116a92e2843929ccd6b28a7a0b036daa
Boghyon Hoffmann <boghyon+github@gmail.com>
2020-12-16 20:47:47 +0100

    Create Component.js

---
+++ webapp/Component.js
@@ -0,0 +1,25 @@
+sap.ui.define([
+  "sap/ui/core/UIComponent",
+  "sap/ui/Device"
+], function (UIComponent, Device) {
+  "use strict";
+
+  return UIComponent.extend("demo.Component", {
+    metadata: {
+      manifest: "json"
+    },
+
+    init: function () {
+      UIComponent.prototype.init.apply(this, arguments);
+      this.setDensity().getRouter().initialize();
+    },
+
+    setDensity: function ({
+      styleClass = Device.system.desktop ? "sapUiSizeCompact" : "sapUiSizeCozy",
+      targetElement = document.body
+    } = {}) {
+      targetElement.classList.add(styleClass);
+      return this;
+    }
+  });
+});
